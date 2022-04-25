f977231c9670b0afc3b15348260dc531807aa429
Mikey <nhmcuong@cmcglobal.vn>
2022-04-25 16:21:50 +0700

    Remove bootstrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import "../styles/globals.scss";
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
-import 'bootstrap/dist/css/bootstrap.css'
 
 import type { AppProps } from "next/app";
 import { store, wrapper } from "../redux/store";
