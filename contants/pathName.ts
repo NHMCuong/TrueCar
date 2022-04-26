34e86144882ed5ed25307ad6b88e9254b3d5435b
Hải <lekhachai9999@gmail.com>
2022-04-26 09:59:18 +0700

    feat: defined paths name (#2)
    Co-authored-by: Hai. Le Khac - CMC Global TFDE3 <lkhai@cmcglobal.vn>

---
+++ contants/pathName.ts
@@ -0,0 +1,20 @@
+export const pathName = {
+  USED: '/used',
+  NEW: '/new',
+  SELL_TRADE: '/sell-trade',
+  EV: '/ev',
+  MILITARY: '/military',
+  BUY_ONLINE: '/buy-online',
+  SITE_MAP: '/site-map',
+  BLOG: '/blog',
+  FAQ: '/faq',
+  CONTACT_US: '/contact-us',
+  DEALERS: '/dealers',
+  PARTNERS: '/partners',
+  TRUE_CARD_INC: '/true-card',
+  TEAM: '/team',
+  CAREERS: '/careers',
+  PRESS: '/press',
+  DEVELOPERS: '/developers',
+  INVESTORS: 'investors'
+}
