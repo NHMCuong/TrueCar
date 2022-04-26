c1a4bb0ddcf6ddfb14952a0f0914fc78347a94a6
Mikey <nhmcuong@cmcglobal.vn>
2022-04-26 15:15:49 +0700

    Create Layout

---
+++ components/Layout/Layout.tsx
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import Navbar from "../Navbar/Navbar";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <>
+      <Head>
+        <title>
+          TrueCar | New and Used Cars for Sale | Car Pricing and Reviews
+        </title>
+        <meta
+          name="description"
+          content="TrueCar lets you control your car-buying journey. Shop new & used cars, sell your car, and find all the vehicle information you need with our research tools."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+      {children}
+    </>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,17 +8,6 @@ import styles from "../styles/Home.module.scss";
 const Home: NextPage = () => {
   return (
     <>
-      <Head>
-        <title>
-          TrueCar | New and Used Cars for Sale | Car Pricing and Reviews
-        </title>
-        <meta
-          name="description"
-          content="TrueCar lets you control your car-buying journey. Shop new & used cars, sell your car, and find all the vehicle information you need with our research tools."
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Navbar />
       <div className={styles.container}>
         <main className={styles.main}></main>
       </div>
